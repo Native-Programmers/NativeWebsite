36e8ed8465c16fc90641f0b8a1f7dd1c5777d36d
Karma8o1 <noumanfaiz.369@gmail.com>
2021-10-05 12:44:09 +0500

    Minor Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hovering/hovering.dart';
+
 
 void main() {
   runApp(MaterialApp(
@@ -19,6 +22,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var search = TextEditingController();
+  final _images = [
+    'assets/back1.png',
+    'assets/back2.png',
+    'assets/back3.png',
+    'assets/back4.png',
+    'assets/back5.png',
+  ];
+  final _key = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -26,77 +38,158 @@ class _HomeState extends State<Home> {
       children: [
 
           Scaffold(
+            key: _key,
             appBar: AppBar(
-              backgroundColor: Color(0xFF2F2750),
+              backgroundColor: const Color(0xFF2F2750),
               elevation: 0,
               title: SizedBox(
                 width: MediaQuery.of(context).size.width,
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(FontAwesomeIcons.phone,
-                          size: 14,
-                          color: Colors.orange,
-                        ),
-                        VerticalDivider(
-                          width:5,
-                        ),
-                        Text("Phone Number",
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                        ),
-                        VerticalDivider(
-                          width:15,
-                        )
-                      ],
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(FontAwesomeIcons.whatsapp,
-                          size: 16,
-                          color: Colors.orange,
+                    HoverWidget(
+                      hoverChild: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(FontAwesomeIcons.phone,
+                            size: 14,
+                            color: Colors.deepOrange,
+                          ),
+                          VerticalDivider(
+                            width:5,
+                          ),
+                          Text("Phone Number",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                          ),
+                          VerticalDivider(
+                            width:15,
+                          )
+                        ],
+                      ),
+                      onHover: (event) {
 
-                        ),
-                        VerticalDivider(
-                          width:5,
-                        ),
-                        Text("Whatsapp Number",
-                          style: TextStyle(
-                            fontSize: 14,
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(FontAwesomeIcons.phone,
+                            size: 14,
+                            color: Colors.orange,
                           ),
-                        ),
-                        VerticalDivider(
-                          width:15,
-                        )
-                      ],
+                          VerticalDivider(
+                            width:5,
+                          ),
+                          Text("Phone Number",
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                          VerticalDivider(
+                            width:15,
+                          )
+                        ],
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(Icons.mail,
-                        size: 16,
-                        color: Colors.orange,
-                        ),
-                        VerticalDivider(
-                          width:5,
-                        ),
-                        Text("Email Address",
-                          style: TextStyle(
-                            fontSize: 14,
+                    HoverWidget(
+                      hoverChild: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(FontAwesomeIcons.whatsapp,
+                            size: 14,
+                            color: Colors.deepOrange,
                           ),
-                        ),
-                        VerticalDivider(
-                          width:15,
-                        )
-                      ],
+                          VerticalDivider(
+                            width:5,
+                          ),
+                          Text("Whatsapp Number",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          VerticalDivider(
+                            width:15,
+                          )
+                        ],
+                      ),
+                      onHover: (event) {
+
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(FontAwesomeIcons.whatsapp,
+                            size: 14,
+                            color: Colors.orange,
+                          ),
+                          VerticalDivider(
+                            width:5,
+                          ),
+                          Text("Whatsapp Number",
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                          VerticalDivider(
+                            width:15,
+                          )
+                        ],
+                      ),
+                    ),
+                    HoverWidget(
+                      hoverChild: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(FontAwesomeIcons.mailBulk,
+                            size: 14,
+                            color: Colors.deepOrange,
+                          ),
+                          VerticalDivider(
+                            width:5,
+                          ),
+                          Text("Email Address",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          VerticalDivider(
+                            width:15,
+                          )
+                        ],
+                      ),
+                      onHover: (event) {
+
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(FontAwesomeIcons.mailBulk,
+                            size: 14,
+                            color: Colors.orange,
+                          ),
+                          VerticalDivider(
+                            width:5,
+                          ),
+                          Text("Email Address",
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                          VerticalDivider(
+                            width:15,
+                          )
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -104,9 +197,78 @@ class _HomeState extends State<Home> {
           ),
             body: Scaffold(
               backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                title: Text("Native Programmers"),
-
+              body: CustomScrollView(
+                slivers: [
+                    // SliverAppBar(
+                    //   title: const Text("Native Programmers"),
+                    //   floating: true,
+                    //   expandedHeight: 50,
+                    //   actions: [
+                    //     Row(
+                    //         children: [
+                    //           SizedBox(
+                    //             width: MediaQuery.of(context).size.width/5,
+                    //             child: TextFormField(
+                    //               controller: search,
+                    //               decoration: InputDecoration(
+                    //                 border: OutlineInputBorder(
+                    //                   borderRadius: BorderRadius.circular(10.0),
+                    //                 ),
+                    //                 focusColor: Colors.white,
+                    //                 fillColor: Colors.white,
+                    //                 filled:true,
+                    //
+                    //               ),
+                    //
+                    //             ),
+                    //
+                    //           ),
+                    //           VerticalDivider(
+                    //             width: 25,
+                    //             color: Colors.transparent,
+                    //           ),
+                    //         ]
+                    //     )
+                    //   ],
+                    // ),
+                  SliverAppBar(
+                    expandedHeight: 350.0,
+                    floating: false,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                        centerTitle: true,
+                        title: const Text("Collapsing Toolbar",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            )),
+                        background: Swiper(
+                          itemCount: _images.length,
+                          itemBuilder: (BuildContext context, int index) => Image.asset(
+                            _images[index],
+                            fit: BoxFit.cover,
+                          ),
+                          autoplay: true,
+                        )),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                            (context, index) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 75,
+                            color: Colors.black12,
+                          ),
+                        ),
+                        childCount: 10),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                          (context, index) => ListTile(title: Text('Item #$index')),
+                      childCount: 1000,
+                    ),
+                  ),
+                ]
               ),
               floatingActionButton: SpeedDial(
                   backgroundColor: Colors.blue[900],
@@ -123,12 +285,12 @@ class _HomeState extends State<Home> {
                       elevation: 5,
                     ),
                     SpeedDialChild(
-                      child: FaIcon(FontAwesomeIcons.phone),
+                      child: const FaIcon(FontAwesomeIcons.phone),
                       label: "Telephone Number",
                       elevation: 5,
                     ),
                     SpeedDialChild(
-                      child: FaIcon(FontAwesomeIcons.whatsapp),
+                      child: const FaIcon(FontAwesomeIcons.whatsapp),
                       label: "Whatsapp Number",
                       elevation: 5,
                     ),
